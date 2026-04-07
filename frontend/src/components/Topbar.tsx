c3a93c8ab1e736146743b77c4fcd606d93a2a585
Xellit <bitt9ra@gmail.com>
2026-04-07 15:54:37 +0300

    ...

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,15 +1,22 @@
-function Topbar(){
-    return(
-        <header className="topbar">
-            <div className="search-box">Search tasks, project, deadlines...</div>
+// Импортируем картинку из папки assets
+import myImage from '../assets/kurs.jpg';
 
-            <div className="topbar-right">
-                <button className="secondary-button">Open Calendar</button>
-                <button className="primary-button">New Task</button>
-                <div className="profile-badge">X</div>
-            </div>
-        </header>
-    );
+function Topbar() {
+  return (
+    <header className="topbar">
+      <div className="search-box">Search tasks, project, deadlines...</div>
+
+      <div className="topbar-right">
+        <button className="secondary-button">Open Calendar</button>
+        <button className="primary-button">New Task</button>
+
+        {/* Аватарка пользователя */}
+        <div className="profile-badge">
+          <img src={myImage} alt="User avatar" className="profile-image" />
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Topbar;
